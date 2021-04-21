ab1775d61754a20f6608c65132232ca4c389b5da
menendbm23 <menendbm@gmail.com>
2021-04-21 13:17:56 -0400

    2nd update

--- Tutorial1/Tutorial1/Program.cs
+++ Tutorial1/Tutorial1/Program.cs
@@ -10,12 +10,15 @@ namespace Tutorial1
     {
         static void Main(string[] args)
         {
-            string characterName = "John";
+            string characterName = "Tom";
             int characterAge;
-            characterAge = 35;
+            characterAge = 25;
 
             Console.WriteLine("There once was a man named " + characterName);
             Console.WriteLine("He was " + characterAge + " years old");
+
+
+            characterName = "Mike";
             Console.WriteLine("He really liked the name " + characterName);
             Console.WriteLine("But didn't like being " + characterAge);
 
