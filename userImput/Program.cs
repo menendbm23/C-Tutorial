bdcedae66ec72da85f007f083dfad7a2cf541c5d
menendbm23 <menendbm@gmail.com>
2021-04-22 14:33:10 -0400

    new program

---
+++ userImput/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace userImput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.Write("Enter your name : ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+
+            Console.Write("Enter your age : ");
+            string age = Console.ReadLine();
+            Console.WriteLine("Hello " + name + " you are " + age + " years old");
+
+
+            Console.Write("Do you want the red pill or blue pill: ");
+            string decision = Console.ReadLine();
+            if (decision == "blue") 
+            {
+                Console.WriteLine(name + " you didnt make it to the matrix. You're dead");
+            } else if (decision == "red") {
+                Console.WriteLine("Congratulations " + name + " you made it into the matrix.");
+            }
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
