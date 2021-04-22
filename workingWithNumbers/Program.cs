d80f5715bd8e971509870f10082215097c87fa94
menendbm23 <menendbm@gmail.com>
2021-04-22 14:14:27 -0400

    new commit

---
+++ workingWithNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace workingWithNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = 6;
+            num++;
+            Console.WriteLine(8.1 + 12.7);
+            Console.WriteLine( (4.1 + 2) * 3 );
+            Console.WriteLine(5 + 7.2);
+            Console.WriteLine((20 - 3) + 7 * 10 + 20 % 3);
+            Console.WriteLine(num + 20);
+
+            Console.WriteLine(Math.Abs(-40));
+            Console.WriteLine(Math.Pow(3, 2));
+            Console.WriteLine(Math.Pow(4.6, 9));
+
+            Console.WriteLine(Math.Sqrt(36));
+
+            Console.WriteLine(Math.Max(4, 90));
+            Console.WriteLine(Math.Min(6, 27));
+            Console.WriteLine(Math.Round(4.3));
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
