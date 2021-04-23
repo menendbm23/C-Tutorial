6acb99befd1d7a10d5df43aa8854ee7fad74cf78
menendbm23 <menendbm@gmail.com>
2021-04-23 11:36:24 -0400

    calc commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the calculator app!");
+
+            int num = Convert.ToInt32("45");
+            Console.WriteLine(num + 6);
+
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter a another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(num1 + num2);
+
+            Console.Write("Enter a number: ");
+            double num3 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter a another number: ");
+            double num4 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(num3 * num4);
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
