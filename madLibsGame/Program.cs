b088d1ce4608a8921c3f4746108f0d4216edcf09
menendbm23 <menendbm@gmail.com>
2021-04-23 11:46:35 -0400

    new update

---
+++ madLibsGame/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace madLibsGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the MadLibs Game!");
+
+
+            string color, pluralNoun, celebrity;
+
+            Console.Write("Enter a color: ");
+            color = Console.ReadLine();
+
+            Console.Write("Enter a plural noun: ");
+            pluralNoun = Console.ReadLine();
+
+            Console.Write("Enter a celebrity: ");
+            celebrity = Console.ReadLine();
+
+            Console.WriteLine("Roses are " + color);
+            Console.WriteLine(pluralNoun + " are blue");
+            Console.WriteLine("I Love " + celebrity);
+
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
